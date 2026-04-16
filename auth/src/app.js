cf2b9b0dbb3af54f1a6116062737d64cc8d5d2a2
rakhi <rakhisingh876.com@gmail.com>
2026-04-16 15:38:03 +0530

    feat(auth): implement JWT authentication, role-based authorization, and user login/register APIs

--- auth/src/app.js
+++ auth/src/app.js
@@ -9,4 +9,9 @@ app.use(cookieParser());
 //Routes
 const authRoutes = require('./routes/auth.routes');
 app.use('/api/auth', authRoutes);
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"Auth Service is up and running"
+        
+})
+})
 module.exports = app;
