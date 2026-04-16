aa9c73b34163aad764292becaa76083558e0a2c8
rakhi <rakhisingh876.com@gmail.com>
2026-04-16 15:39:04 +0530

    feat(order): implement order creation, order retrieval, status updates, and cancellation flow

--- order/src/app.js
+++ order/src/app.js
@@ -8,5 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/orders', orderRoutes);
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"Order Service is up and running"
+    })
+})
 
 module.exports = app;
